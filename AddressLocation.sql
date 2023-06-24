fb59d503cd5279372e6c1f47196868e9c4345444
zinmarnwe3 <127071104+zinmarnwe3@users.noreply.github.com>
2023-06-24 22:42:18 +0630

    upload script file

---
+++ AddressLocation.sql
@@ -0,0 +1,11 @@
+USE AddressLocation; -- Replace 'AddressLocation' with your database name
+
+CREATE TABLE Address (
+    AddressID INT PRIMARY KEY,
+    Building VARCHAR(100),
+    Level VARCHAR(50),
+    Room VARCHAR(50),
+    Carpark VARCHAR(50),
+    Lobby VARCHAR(50),
+    Pantry VARCHAR(50)
+);
